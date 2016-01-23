aab65941b63b1c8132ae60bf6ebf22e81b88617d
Ona Boseroy <o.boseroy@gmail.com>
2016-01-23 17:13:19 -0500

    adds CSS styling for a footer that stays at the bottom of the page and a few other things

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,16 @@
 console.log('linked');
 
+if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+  var msViewportStyle = document.createElement('style')
+  msViewportStyle.appendChild(
+    document.createTextNode(
+      '@-ms-viewport{width:auto!important}'
+    )
+  )
+  document.querySelector('head').appendChild(msViewportStyle)
+}
+
 $(document).ready(function(){
- 
 
   acts = [
     "VOTE!",
